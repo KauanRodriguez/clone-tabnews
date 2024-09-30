f85d8c659a435ad9558b48c561636fb1f5c5895c
Kauan Rodrigues <139260951+KauanRodriguez@users.noreply.github.com>
2024-09-30 15:47:39 +0000

    adiciona scripts `link:check` e `link:fix`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Hello world ! 😉</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
-
 export default Home;
