5ca9c00b3a29c4aec9b16f7e79d6adac79890929
Kauan Rodrigues <139260951+KauanRodriguez@users.noreply.github.com>
2024-09-30 15:44:11 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Hello world ! 😉</h1>
+  return <h1>Hello world ! 😉</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
